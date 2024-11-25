8b25a4f4cff258a285a47f5fcc6a225c0936dee7
ayumim42009 <153568294+ayumim42009@users.noreply.github.com>
2024-11-24 16:22:29 -0800

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTests
+{
+    [TestClass]
+    public class SucccessfulLogin
+    {
+        private static IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private static string baseURL;
+        private bool acceptNextAlert = true;
+        
+        [ClassInitialize]
+        public static void InitializeClass(TestContext testContext)
+        {
+            driver = new ChromeDriver();
+            baseURL = "https://www.google.com/";
+        }
+        
+        [ClassCleanup]
+        public static void CleanupClass()
+        {
+            try
+            {
+                //driver.Quit();// quit does not close the window
+                driver.Close();
+                driver.Dispose();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+        }
+        
+        [TestInitialize]
+        public void InitializeTest()
+        {
+            verificationErrors = new StringBuilder();
+        }
+        
+        [TestCleanup]
+        public void CleanupTest()
+        {
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+        
+        [TestMethod]
+        public void TheSucccessfulLoginTest()
+        {
+            driver.Navigate().GoToUrl("https://letsusedata.com/index.html");
+            driver.FindElement(By.Id("txtUser")).Click();
+            driver.FindElement(By.Id("txtUser")).Clear();
+            driver.FindElement(By.Id("txtUser")).SendKeys("test1");
+            driver.FindElement(By.Id("txtPassword")).Click();
+            driver.FindElement(By.Id("txtPassword")).Clear();
+            driver.FindElement(By.Id("txtPassword")).SendKeys("Test12456");
+            driver.FindElement(By.Id("javascriptLogin")).Click();
+            driver.Navigate().GoToUrl("https://letsusedata.com/CourseSelection.html");
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+        
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+        
+        private string CloseAlertAndGetItsText() {
+            try {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert) {
+                    alert.Accept();
+                } else {
+                    alert.Dismiss();
+                }
+                return alertText;
+            } finally {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
